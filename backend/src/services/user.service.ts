c9c715e4d7500ffaf2910bb02fd96f76efc0f17c
Kodomoppoi <jcmm.esft@gmail.com>
2025-05-15 21:53:48 -0300

    correção do conflito

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,28 +1,5 @@
 import { User } from '../models/user.model';
-import fs from 'fs';
-import path from 'path';
 import { prisma, Prisma } from '../lib/prisma';
-
-
-// TO DO: alterar a logica deste arquivo para em vez de utilizar o fs, utilizar um banco de dados no postgreSQL com prisma
-// 
-const dataPath = path.join(__dirname, '../../src/data/users.json');
-
-// Helper function to read users
-const readUsers = (): User[] => {
-  try {
-    const data = fs.readFileSync(dataPath, 'utf-8');
-    return JSON.parse(data);
-  } catch (error) {
-    console.error('Erro ao ler users:', error);
-    return [];
-  }
-};
-
-// Helper function to write users
-const writeUsers = (users: User[]) => {
-  fs.writeFileSync(dataPath, JSON.stringify(users, null, 2), 'utf-8');
-};
 
 export const UserService = {
   getAllUsers: async (): Promise<User[]> => {
