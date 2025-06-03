c7aa2df2f51b1dc041f36d1ac91ca38f1a218a53
renannOgomes <renannpop11@gmail.com>
2025-06-03 09:05:33 -0300

    Update prisma schema and add itinerary and activitys services+controller+router

--- backend/src/models/user.planoViagem.ts
+++ backend/src/models/user.planoViagem.ts
@@ -1,10 +1,10 @@
-export interface PlanoViagem {
-  id: number;
-  nome: string;
-  destino?: string | null;
-  dataInicio?: Date | null;
-  dataFim?: Date | null;
+// export interface PlanoViagem {
+//   id: number;
+//   nome: string;
+//   destino?: string | null;
+//   dataInicio?: Date | null;
+//   dataFim?: Date | null;
 
-  createdAt: Date;
-  updatedAt: Date;
-}
+//   createdAt: Date;
+//   updatedAt: Date;
+// }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,18 +1,15 @@
 import { Router } from 'express';
-import { UserController } from '../controllers/user.controller';
-import { UserPreferencesController } from '../controllers/userPreferences.controller';
-import { TravelInterestsController } from '../controllers/travelInterests.controller';
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from '../controllers/user.controller';
 const router = Router();
 
-router.get('/', (req, res) => UserController.getAllUsers(req, res));
-router.get('/:id', (req, res) => UserController.getUserById(req, res));
-router.post('/', (req, res) => UserController.createUser(req, res));
-router.put('/:id', (req, res) => UserController.updateUser(req, res));
-router.delete('/:id', (req, res) => UserController.deleteUser(req, res));
-router.get('/:id/preferences', UserPreferencesController.getUserTravelPreferencesByUserId);
-router.post('/:id/preferences', UserPreferencesController.saveUserTravelPreferences);
-router.put('/:id/preferences', UserPreferencesController.saveUserTravelPreferences);
-router.get('/:id/interests', TravelInterestsController.getUserTravelInterestsByUserId);
-router.post('/:id/interests', TravelInterestsController.saveUserTravelInterests);
-router.put('/:id/interests', TravelInterestsController.saveUserTravelInterests);
+//Create
+router.post('/', createUser);
+//Read
+router.get('/', getUsers);
+router.get('/:userId', getUserById);
+// Update
+router.put('/:userId', updateUser);
+// Delete
+router.delete('/:userId', deleteUser);
+
 export default router;
