36b21e83559f620ca09aa6831b69aa9cf7c12ab6
Bruno Duarte <bbduarte@live.com>
2025-06-08 18:36:15 -0300

    feat: adiciona documentação das rotas

--- backend/src/routes/activity.routes.ts
+++ backend/src/routes/activity.routes.ts
@@ -8,13 +8,9 @@ import {
 
 const router = Router({ mergeParams: true });
 
-// Create
 router.post('/', createActivity);
-// Read
 router.get('/', getActivities);
-// Update
 router.put('/:activityId', updateActivity);
-// Delete
 router.delete('/:activityId', deleteActivity);
 
 export default router;
