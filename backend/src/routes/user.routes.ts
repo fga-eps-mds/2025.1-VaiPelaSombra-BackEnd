8642af2d54edbe9bdff8c24da656a5616ec5145d
Bruno Duarte <bbduarte@live.com>
2025-06-08 18:36:15 -0300

    feat: adiciona documentação das rotas

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -6,7 +6,6 @@ import {
   getUsers,
   updateUser,
 } from '../controllers/user.controller';
-
 import itineraryRouter from './itinerary.routes';
 import travelPreferenceRouter from './travelPreference.routes';
 const router = Router();
@@ -18,16 +17,4 @@ router.delete('/:userId', deleteUser);
 router.use('/:userId/itineraries', itineraryRouter);
 router.use('/:userId/travel-preferences', travelPreferenceRouter);
 
-//Create
-router.post('/', createUser);
-//Read
-router.get('/', getUsers);
-router.get('/:userId', getUserById);
-// Update
-router.put('/:userId', updateUser);
-// Delete
-router.delete('/:userId', deleteUser);
-
-router.use('/:userId/itineraries', itineraryRouter);
-router.use('/:userId/preferences', travelPreferenceRouter);
 export default router;
