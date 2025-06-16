ed52496138e2601fab6a996a03fe0998198b3fb5
Estevao468 <estevaomagalhaeestevao@gmail.com>
2025-06-16 18:47:58 -0300

    Retirada do sistema de autenticacao para testar se precisa de usuario logado

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,7 +4,7 @@ import travelPreferenceRouter from './travelPreference.routes';
 import { UserController } from '../controllers/user.controller';
 import { UserPreferencesController } from '../controllers/userPreferences.controller';
 import { TravelInterestsController } from '../controllers/travelInterests.controller';
-import { authMiddleware } from '../middlewares/error.middleware';
+//import { authMiddleware } from '../middlewares/error.middleware';
 
 const { getUserProfile, updateUserProfile } = UserController;
 const router = Router();
@@ -20,9 +20,9 @@ router.put('/:id/preferences', UserPreferencesController.saveUserTravelPreferenc
 router.get('/:id/interests', TravelInterestsController.getUserTravelInterestsByUserId);
 router.post('/:id/interests', TravelInterestsController.saveUserTravelInterests);
 router.put('/:id/interests', TravelInterestsController.saveUserTravelInterests);
-router.get('/me', authMiddleware, getUserProfile);
-router.put('/me', authMiddleware, updateUserProfile);
 router.use('/:userId/itineraries', itineraryRouter);
 router.use('/:userId/travel-preferences', travelPreferenceRouter);
+router.get('/:id/profile', getUserProfile); // router.get('/me', authMiddleware, getUserProfile);
+router.put('/:id/profile', updateUserProfile); // router.put('/me', authMiddleware, updateUserProfile);
 
 export default router;
