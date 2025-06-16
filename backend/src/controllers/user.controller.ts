00594f84cc4ba4cbd90db884926ef6c085825206
Estevao468 <estevaomagalhaeestevao@gmail.com>
2025-06-16 18:47:58 -0300

    Retirada do sistema de autenticacao para testar se precisa de usuario logado

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -54,9 +54,9 @@ export const UserController = {
 
     res.status(204).send();
   },
-  getUserProfile: async (req: AuthenticatedRequest, res: Response): Promise<void> => {
+  getUserProfile: async (req: Request, res: Response): Promise<void> => { //req: AuthenticatedRequest
     try {
-      const userId = Number(req.user.id);
+      const userId = Number(req.params.id); //const userId = Number(req.user.id);
       const user = await UserService.getUserById(userId);
       if (!user) {
         res.status(404).json({ message: 'User not found' });
@@ -67,9 +67,9 @@ export const UserController = {
       res.status(500).json({ message: 'Error fetching user profile', error });
     }
   },
-  updateUserProfile: async (req: AuthenticatedRequest, res: Response): Promise<void> => {
+  updateUserProfile: async (req: Request, res: Response): Promise<void> => { // req: AuthenticatedRequest
     try {
-      const userId = Number(req.user.id);
+      const userId = Number(req.params.id); // const userId = Number(req.user.id);
       const userData = req.body;
       const updatedUser = await UserService.updateUser(userId, userData);
       if (userData.travelPreferencesData) {
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { UserController } from '../controllers/user.controller';
 import { UserPreferencesController } from '../controllers/userPreferences.controller';
 import { TravelInterestsController } from '../controllers/travelInterests.controller';
-import { authMiddleware } from '../middlewares/error.middleware';
+//import { authMiddleware } from '../middlewares/error.middleware';
 
 const { getUserProfile, updateUserProfile } = UserController;
 const router = Router();
@@ -18,7 +18,7 @@ router.put('/:id/preferences', UserPreferencesController.saveUserTravelPreferenc
 router.get('/:id/interests', TravelInterestsController.getUserTravelInterestsByUserId);
 router.post('/:id/interests', TravelInterestsController.saveUserTravelInterests);
 router.put('/:id/interests', TravelInterestsController.saveUserTravelInterests);
-router.get('/me', authMiddleware, getUserProfile);
-router.put('/me', authMiddleware, updateUserProfile);
+router.get('/:id/profile', getUserProfile); // router.get('/me', authMiddleware, getUserProfile);
+router.put('/:id/profile', updateUserProfile); // router.put('/me', authMiddleware, updateUserProfile);
 
 export default router;
