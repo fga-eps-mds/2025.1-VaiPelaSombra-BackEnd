90cb735086943d13bbbc35ca27617ddb4aa18211
Estevao468 <estevaomagalhaeestevao@gmail.com>
2025-06-26 12:42:48 -0300

    Atualização dos teste unitarios do backend US48

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { UserService } from '../services/user.service';
-import { AuthenticatedRequest } from '../middlewares/error.middleware';
+//import { AuthenticatedRequest } from '../middlewares/error.middleware';
 
 export const UserController = {
   getAllUsers: async (req: Request, res: Response): Promise<void> => {
@@ -54,38 +54,47 @@ export const UserController = {
 
     res.status(204).send();
   },
+
   getUserProfile: async (req: Request, res: Response): Promise<void> => {
     try {
-      const userId = Number(req.params.id); //const userId = Number(req.user.id);
+      const userId = Number(req.params.id); // const userId = Number(req.user.id);
       const user = await UserService.getUserById(userId);
-    if (!user) {
-      res.status(404).json({ message: 'User not found' });
-      return;
+      if (!user) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({
+        message: 'Error fetching user profile',
+        error: error instanceof Error ? error.message : error,
+      });
     }
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching user profile', error });
-  }
-},
-
+  },
 
   updateUserProfile: async (req: Request, res: Response): Promise<void> => {
     try {
       const userId = Number(req.params.id);
       const userData = req.body;
+
       const updatedUser = await UserService.updateUser(userId, userData);
+      if (!updatedUser) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+
       if (userData.travelPreferencesData) {
         await UserService.updateTravelPreferences(userId, userData.travelPreferencesData);
       }
-    if (!updatedUser) {
-      res.status(404).json({ message: 'User not found' });
-      return;
-    }
-    const userUpdated = await UserService.getUserById(userId);
-    res.status(200).json(userUpdated);
+
+      const userUpdated = await UserService.getUserById(userId);
+      res.status(200).json(userUpdated);
     } catch (error) {
-      res.status(500).json({ message: 'Erro updating user profile', error });
+      console.error('Erro inesperado:', error);
+      res.status(500).json({
+        message: 'Erro updating user profile',
+        error: error instanceof Error ? error.message : error,
+      });
     }
   },
 };
-
