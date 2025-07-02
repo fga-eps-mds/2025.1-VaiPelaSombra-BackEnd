714ec43d8bc5fc4b7fc8e9b3e339629960ba0a66
Bruno Duarte <bbduarte@live.com>
2025-07-02 11:40:51 -0300

    #22 reactor: altera o controlador de itinerários para seguir o mesmo padrão do controlador de destinos essa alterção foi feita para melhorar a testabilidade do código

--- backend/src/routes/itinerary.routes.ts
+++ backend/src/routes/itinerary.routes.ts
@@ -1,19 +1,17 @@
 import { Router } from 'express';
-import {
-  createItinerary,
-  deleteItinerary,
-  findByUserId,
-  // findByUserItineraryId,
-  updateItinerary,
-} from '../controllers/itinerary.controller';
-
 import activityRouter from './activity.routes';
+import { ItineraryController } from '../controllers/itinerary.controller';
+import { ItineraryService } from '../services/itinerary.service';
+
+const itineraryService = new ItineraryService();
+const itineraryController = new ItineraryController(itineraryService);
+
 const router = Router({ mergeParams: true });
 
-router.post('/', createItinerary);
-router.get('/', findByUserId);
-//router.get('/:itineraryId', findByItineraryId);
-router.put('/:itineraryId', updateItinerary);
-router.delete('/:itineraryId', deleteItinerary);
+router.post('/', itineraryController.createItinerary);
+router.get('/', itineraryController.findByUserId);
+router.put('/:itineraryId', itineraryController.updateItinerary);
+router.delete('/:itineraryId', itineraryController.deleteItinerary);
+
 router.use('/:itineraryId/activities', activityRouter);
 export default router;
