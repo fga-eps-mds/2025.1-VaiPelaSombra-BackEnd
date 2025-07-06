234842bbd2e230a1ad81e8d1bcb96608c9ec05c0
Bruno Duarte <bbduarte@live.com>
2025-07-06 19:19:17 -0300

    #22 feat: adiciona configuração cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,12 @@ const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.bundle.yaml'));
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
