ff3307979f9779354b5439cdb32f9c92db06ac52
Pablo Guilherme de Jesus Batista Silva <200025791@aluno.unb.br>
2025-07-11 16:20:00 -0300

    correção app.ts diminuindo carga de memoria

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-// import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
@@ -9,7 +8,7 @@ import travelInterestsRouter from './routes/travelInterests.routes';
 import transportRouter from './routes/transport.routes';
 import destinationRouter from './routes/destination.routes';
 import homeRouter from './routes/home.routes';
-//import loginRouter from './routes/login.routes';
+// import loginRouter from './routes/login.routes';
 import { errorHandler } from './errors/errorHandler';
 import authRouter from './routes/auth.routes';
 import cookieParser from 'cookie-parser';
@@ -28,24 +27,28 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-// Swagger
-const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.bundle.yaml'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Swagger (opcional em produção)
+
+try {
+  const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.bundle.yaml'));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} catch (error: any) {
+  console.warn('⚠️ Swagger não carregado:', error.message);
+}
 
 // Rotas
 app.use('/interests', travelInterestsRouter);
 app.use('/users', userRouter);
 app.use('/travel-interests', travelInterestsRouter);
 app.use('/transports', transportRouter);
-app.use('/interests', travelInterestsRouter);
 app.use('/destinations', destinationRouter);
 app.use('/home', homeRouter);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use('/auth', authRouter);
-app.use('/transports', transportRouter);
-// app.use('/login', loginRouter);
 app.use('/itineraries', intineraryRouter);
 app.use('/email', emailRouter);
+
+// Error Handler
 app.use(errorHandler);
 
 export default app;
